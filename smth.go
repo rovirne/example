449cc5d3c2a0514a7f8615900aba8cffd537adb7
Илья Смоленцев <ilasmolencev@MacBook-Air-Ila.local>
2026-03-18 09:31:45 +0100

    basic functionality

---
+++ smth.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("send to closed channel  ", r)
+		}
+	}()
+	res := make(chan int, 2)
+	res <- 1
+	res <- 2
+	close(res)
+	v, ok := <-res
+	fmt.Println(v, ok)
+	fmt.Println(<-res)
+	fmt.Println(<-res)
+	fmt.Println(<-res)
+}
